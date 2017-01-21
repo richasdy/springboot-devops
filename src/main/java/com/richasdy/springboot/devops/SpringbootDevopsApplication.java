dfebe479760c74c822bb5605fc10623f378e163f
Donni Richasdy <donni.richasdy@gmail.com>
2017-01-21 13:12:39 +0700

    first commit

---
+++ src/main/java/com/richasdy/springboot/devops/SpringbootDevopsApplication.java
@@ -0,0 +1,12 @@
+package com.richasdy.springboot.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevopsApplication.class, args);
+	}
+}
