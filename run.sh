9318e92647aa38acac6870bf1be11e4df8432919
Donni Richasdy <donni.richasdy@gmail.com>
2017-01-24 14:22:31 +0700

    another trial and refinement

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
 
 # packaging and run --> basic maven
-# mvn package && java -jar target/springboot-devops-0.0.1-SNAPSHOT.jar
+#mvn package && java -jar target/springboot-devops-0.0.1-SNAPSHOT.jar
 
 # docker
 # package for docker
 # -DpushImage for push image to dockerhub
-# mvn package docker:build -DpushImage
+#mvn package docker:build -DpushImage
 mvn package docker:build
 
 # preparation, remove if docker container exist
+# error for the first time, container not exist
 docker stop springboot-devops
 docker rm springboot-devops
 
