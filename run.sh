2c34ec2be849b8a8bdd247b67e324281adca8354
Donni Richasdy <donni.richasdy@gmail.com>
2017-01-24 14:10:20 +0700

    update run command

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -e
-#package and run
-mvn package && java -jar target/springboot-devops-0.0.1-SNAPSHOT.jar
+
+# packaging and run --> basic maven
+# mvn package && java -jar target/springboot-devops-0.0.1-SNAPSHOT.jar
+
+# docker
+# package for docker
+# -DpushImage for push image to dockerhub
+# mvn package docker:build -DpushImage
+mvn package docker:build
+
+# run docker
+docker run -d -p 8080:8080 --name springboot-devops -t richasdy/springboot-devops 
